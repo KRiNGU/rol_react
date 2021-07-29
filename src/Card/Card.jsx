2a353ab6ae4113d5f5a278180f836f6e6e1200ed
Alexandr Lobashov <viper.489@inbox.ru>
2021-07-29 18:36:00 +0300

    card reworked

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,46 +1,33 @@
 import "./Card.css";
-import React from "react";
+import React, { useState } from "react";
 
-export default class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      liked: false
-    };
-  }
-
-  _toggleLike() {
-    this.setState((prevState) => ({
-      liked: !prevState.liked,
-    }));
-  }
-
-  render() {
-    const liked = this.state.liked
-      ? "card__like-button card__like-button_active"
-      : "card__like-button";
-    return (
-      <article className="card">
+export default function Card(props) {
+  const [liked, setLiked] = useState(false);
+  return (
+    <article className="card">
+      <button
+        className="card__delete-button"
+        onClick={props.onClick}
+        aria-label="Удалить карточку"
+      ></button>
+      <img
+        src={props.link}
+        alt={props.title}
+        className="card__foto"
+        onClick={props.onImgClick}
+      ></img>
+      <div className="card__description">
+        <h2 className="card__title">{props.title}</h2>
         <button
-          className="card__delete-button"
-          onClick={this.props.onClick}
-          aria-label="Удалить карточку"
+          className={
+            liked
+              ? "card__like-button card__like-button_active"
+              : "card__like-button"
+          }
+          onClick={() => setLiked(!liked)}
+          aria-label="Поставить лайк"
         ></button>
-        <img
-          src={this.props.link}
-          alt={this.props.title}
-          className="card__foto"
-          onClick={this.props.onImgClick}
-        ></img>
-        <div className="card__description">
-          <h2 className="card__title">{this.props.title}</h2>
-          <button
-            className={liked}
-            onClick={() => this._toggleLike()}
-            aria-label="Поставить лайк"
-          ></button>
-        </div>
-      </article>
-    );
-  }
+      </div>
+    </article>
+  );
 }
